b9734891c5d116bf248ae0b75e39e33ae2edcf20
unknown <jorgecardozosilvio@gmail.com>
2020-06-14 15:00:42 -0300

    Agregamos code para reintetar reconectar a la DB, si es que se pierde la conexion

--- src/database.js
+++ src/database.js
@@ -58,19 +58,27 @@ var db_config = {
   var mysqlConnection;
   
   function handleDisconnect() {
+    console.error('Start Conexion');
     mysqlConnection = mysql.createConnection(db_config); // Recreate the connection, since
                                                         // the old one cannot be reused.
-  
     mysqlConnection.connect(function(err) {              // The server is either down
       if(err) {                                     // or restarting (takes a while sometimes).
-        console.log('error when connecting to db:', err);
+        console.log('Error cuando se intenta conectar a la DB:', err);
         setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      }else{
+        console.log('Conexion SUCCESS to DB');
       }                                     // to avoid a hot loop, and to allow our node script to
     });                                     // process asynchronous requests in the meantime.
                                             // If you're also serving http, display a 503 error.
     mysqlConnection.on('error', function(err) {
         console.log('db error', err);
-        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+        /*if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }*/
+
+        if(err.fatal) { // Connection to the MySQL server is usually
             handleDisconnect();                         // lost due to either server restart, or a
         } else {                                      // connnection idle timeout (the wait_timeout
             throw err;                                  // server variable configures this)
